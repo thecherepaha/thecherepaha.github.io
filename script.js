0c7822808a5c0718736ffc6d33d972dfd3f99dd4
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-02-23 18:57:03 +0600

    sending data to the google sheets

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ document.getElementById("action_type").oninput = function(){
     document.getElementById("status").style = "background-color: green;border-radius: 5px;text-align: center; color: white";
 }
 
-function createRandomNumber(){
-    
+
+
+function sendData(){
+    var patient = document.getElementById("patient_name").value;
+    var treatment = document.getElementById("treatment_type").value;
+    var agent = document.getElementById("agent_name").value;
+    var plan = document.getElementById("plan_type").value;
+    var employer = document.getElementById("employer_name").value;
+    var pat_request_type = document.getElementById("pat_request_type").value;
+    var pat_request_subtype = document.getElementById("pat_request_subtype").value;
+    var action_type = document.getElementById("action_type").value;
+
+    var data = [patient, treatment, agent, plan, employer, pat_request_type, pat_request_subtype, action_type];
+
+
+    $.post("script.php", {
+        data: data,
+        
+    }, function(data) {
+        alert("Your data sent to google sheets!");
+    });
+
 }
---
+++ script.php
@@ -0,0 +1,22 @@
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php'; // google-api-php-client path
+    //echo json_encode($_SERVER['DOCUMENT_ROOT']);
+    
+    $client = new Google_Client();
+    $client->setApplicationName('diplom');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS);
+    $client->setAuthConfig('params.json');
+    $client->setAccessType('offline');
+
+    $array = $_POST["data"];
+    $service = new Google_Service_Sheets($client);
+
+    $value = new Google_Service_Sheets_ValueRange();
+    $value->setValues(["values" => $array]);
+    $conf = ["valueInputOption" => "USER_ENTERED"];
+    $service->spreadsheets_values->append("1pb4tyfXlLxjIJcRLcJ5cyOmlgX9ZPjKID_3KA-p7jDQ", "Patients!A2:A", $value, $conf);
+
+
+?>
