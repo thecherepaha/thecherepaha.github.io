6217bfd20083462e1b454400533d459dc2e801c5
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-02-23 16:58:30 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// window.onload = function(){
+//     getData();
+// }
+
+document.getElementById("action_type").oninput = function(){
+    document.getElementById("status").value = "Active";
+    document.getElementById("status").style = "background-color: green;border-radius: 5px;text-align: center; color: white";
+}
+
+function createRandomNumber(){
+    
+}
